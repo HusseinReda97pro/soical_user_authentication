1a287472a5e956d765a4acace1ea0eedb373a49f
Pro.Hussein.Reda <hreda@arabdt.com>
2022-11-07 16:15:46 +0200

    update read me

--- lib/soical_user_authentication.dart
+++ lib/soical_user_authentication.dart
@@ -2,7 +2,7 @@ library soical_user_authentication;
 
 export 'src/models/soical_user.dart';
 export 'src/provider/soical_user_provider.dart';
-export 'src/models/soical_user.dart';
+export 'src/models/user_provider_enum.dart';
 
 export 'src/provider/soical_user_provider.dart';
 export 'src/repository/soical_user_repository.dart';

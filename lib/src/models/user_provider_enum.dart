30fb4fa38c9e3573b69eb4e85dd42ea7cb23f382
Pro.Hussein.Reda <hreda@arabdt.com>
2022-11-07 17:19:31 +0200

    add toUserProvider

--- lib/src/models/user_provider_enum.dart
+++ lib/src/models/user_provider_enum.dart
@@ -1,1 +1,19 @@
 enum UserProvider { facebook, google, emailPassword }
+
+extension on UserProvider {
+  String toText() {
+    if (this == UserProvider.facebook) return 'facebook';
+    if (this == UserProvider.google) return 'google';
+    if (this == UserProvider.emailPassword) return 'emailPassword';
+    return 'emailPassword';
+  }
+}
+
+extension on String {
+  toUserProvider() {
+    if (this == 'facebook') return UserProvider.facebook;
+    if (this == 'google') return UserProvider.google;
+    if (this == 'emailPassword') return UserProvider.emailPassword;
+    return 'emailPassword';
+  }
+}
